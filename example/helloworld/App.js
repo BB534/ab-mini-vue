f7742e32d318555d8ca5b3fe2914550cfcf8c1eb
ab <2553400824@qq.com>
2022-08-07 22:26:14 +0800

    processElement的实现,h函数的实现

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -1,7 +1,10 @@
 import { h } from '../../lib/ab-mini-vue-esm.js'
 export const App = {
 	render() {
-		return h('div', 'hi,' + this.msg)
+		return h('div', { id: 'root' }, [
+			h('p', { class: 'red' }, 'red'),
+			h('p', { class: 'blue' }, 'blue')
+		])
 	},
 	setup() {
 		return {
--- src/rumtime-core/render.ts
+++ src/rumtime-core/render.ts
@@ -1,4 +1,5 @@
-import { createComponentInstance, setupComponent } from './component'
+import { isObject } from './../shared/index';
+import { createComponentInstance, setupComponent } from './component';
 export function render(vnode: any, container: any) {
   // patch
   patch(vnode, container)
@@ -6,14 +7,40 @@ export function render(vnode: any, container: any) {
 
 function patch(vnode: any, container: any) {
   // patch -> vnode.type  组件类型 ？ el类型
-  processComponent(vnode, container)
-
-  // TODO 判断vnode是不是一个element
-  // 是一个element 那么就处理element
-  processElement()
+  if (typeof vnode.type === 'string') {
+    processElement(vnode, container)
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container)
+  }
 }
 
-function processElement() { }
+function processElement(vnode, container) {
+  // 处理el
+  mountElement(vnode, container)
+}
+
+function mountElement(vnode, container) {
+  const { type, props, children } = vnode
+  const el = document.createElement(type) as Element
+  // 如果是字符串类型的子节点,直接挂载,如果是数组类型继续遍历然后patch
+
+  if (typeof children === 'string') {
+    el.textContent = children
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el)
+  }
+  for (const key in props) {
+    const val = props[key]
+    el.setAttribute(key, val)
+  }
+  container.append(el)
+}
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach(v => {
+    patch(v, container)
+  })
+}
 
 function processComponent(vnode, container) {
   // 组件类型，挂载component
