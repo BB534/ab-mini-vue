8c0cdb9524b5ed43384ed45895380f24ce71a2ce
ab <2553400824@qq.com>
2022-08-12 00:31:20 +0800

    provide && inject 的最终实现

--- example/helloworld/Inject.js
+++ example/helloworld/Inject.js
@@ -14,6 +14,7 @@ const ProvideTow = {
 	setup(props) {
 		provide('foo', 'ProvideTow-value')
 		// 自己组件内也获取父组件的注入数据,正常情况应该是获取到的---fooval,这里因为初始化时是暴力初始化成了{}所以获取到的是自己的
+		// 所以为了当前组件重新定义数据后,通过注入拿到的数据应该还是父级组件的，利用原型链的特点实现
 		const foo = inject('foo')
 		return {
 			foo
@@ -28,12 +29,21 @@ const Custom = {
 	setup(props) {
 		const foo = inject('foo')
 		const name = inject('abi')
+		const defaulValue = inject('baz', '没有值，返回默认值')
+		const defaluFn = inject('baz', () => '默认值是一个函数')
 		return {
 			foo,
-			name
+			name,
+			defaulValue,
+			defaluFn
 		}
 	},
 	render() {
-		return h('div', {}, [h('h1', {}, 'foo:' + this.foo), h('h1', {}, 'name:' + this.name)])
+		return h('div', {}, [
+			h('h1', {}, 'foo:' + this.foo),
+			h('h1', {}, 'name:' + this.name),
+			h('h1', {}, '如果值不存在返回:' + this.defaulValue),
+			h('h1', {}, '默认值是一个函数:' + this.defaluFn)
+		])
 	}
 }
--- src/rumtime-core/apiInject.ts
+++ src/rumtime-core/apiInject.ts
@@ -3,14 +3,33 @@ export function provide(key, value) {
   // 因为provide只能在setup中使用,可以直接使用写好的获取当前组件实例方法getCurrentInstance
   const instance: any = getCurrentInstance()
   if (instance) {
-    instance.provides[key] = value
+    let { provides } = instance
+    // 初始化时改变当前的原型链指向父级,但是可能在使用provide时会多次调用，所以我们需要用一个条件限制只初始化一次。
+    const parentProvides = instance.parent.provides
+    // 一进来初始化当前的provides 如果没有自己的值,那么就是和父级的provides是相等的,这时我们就改变原型链
+    // 当执行给provides[key]赋值后,当前实例的provides就多了一个属性,所以就不会走到条件内去
+    // 先改变原型链然后再赋值给他
+    if (provides === parentProvides) {
+      provides = (instance.provides = Object.create(parentProvides))
+    }
+    provides[key] = value
   }
 }
 
-export function inject(key) {
+export function inject(key, defaultValue) {
   // 从当前子节点获取父节点上的provides
   const instance: any = getCurrentInstance()
   if (instance) {
-    return instance.parent.provides[key]
+    // 需求:如果当前的inject没有获取到,用户可以自定义一个默认返回值
+    // 需求2:defaultValue可以是一个函数
+    const provides = instance.parent.provides
+    if (key in provides) {
+      return provides[key]
+    } else if (defaultValue) {
+      if (typeof defaultValue === 'function') {
+        return defaultValue()
+      }
+      return defaultValue
+    }
   }
 }
